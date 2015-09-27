290b857259d04cf25fb40dc9fc37e29ad6441710
quattromani <max@maxquattromani.com>
2015-09-27 11:03:29 -0600

    big update - still needs cleaned

---
+++ src/js/scroll-to.js
@@ -0,0 +1,19 @@
+$('a[href*=#]:not([href=#])').click(function() {
+
+  var windowWidth = $("body").width();
+  var offset = windowWidth > 1020 ? $('nav') : $('header[role=banner]');
+  var offset_height = offset.outerHeight();
+
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    || location.hostname == this.hostname) {
+
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+     if (target.length) {
+       $('html,body').animate({
+           scrollTop: target.offset().top - headerHeight
+      }, 1000);
+      return false;
+    }
+  }
+});
