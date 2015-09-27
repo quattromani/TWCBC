a2e8c292fc2b0711d1d2b0268a8e98b40336ac79
quattromani <max@maxquattromani.com>
2015-09-27 13:23:00 -0600

    adding assemble

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,141 +1,170 @@
 'use strict';
 
-// # Globbing
-// for performance reasons we're only matching one level down:
-// '<%= config.src %>/templates/pages/{,*/}*.hbs'
-// use this if you want to match all subfolders:
-// '<%= config.src %>/templates/pages/**/*.hbs'
-
 module.exports = function(grunt) {
 
-	require('time-grunt')(grunt);
-	require('load-grunt-tasks')(grunt);
+  require('time-grunt')(grunt);
+  require('load-grunt-tasks')(grunt);
 
   // Project configuration.
   grunt.initConfig({
 
-  	config: {
-  		src: 'src',
-  		dist: 'dist'
-  	},
+    config: {
+      src: 'src',
+      dist: 'dist'
+    },
 
     concat: {
       options: {
         separator: ';'
       },
       dist: {
-        src: ['src/js/*.js'],
-        dest: 'dist/js/scripts.min.js'
+        src: ['<%= config.src %>/js/*.js'],
+        dest: '<%= config.dist %>/js/scripts.js'
       }
     },
 
     uglify: {
       build: {
-        src: 'dist/js/scripts.min.js',
-        dest: 'dist/js/scripts.min.js'
+        src: '<%= config.dist %>/js/scripts.js',
+        dest: '<%= config.dist %>/js/scripts.min.js'
       }
     },
 
-  	sass: {
-  		dist: {
-  			files: {
-  				'<%= config.src %>/css/build/styles.css' : '<%= config.src %>/css/scss/styles.scss'
-  			}
-  		}
-  	},
+    sass: {
+      dist: {
+        files: {
+          '<%= config.src %>/css/build/styles.css' : '<%= config.src %>/css/scss/styles.scss'
+        }
+      }
+    },
 
-  	watch: {
-  		assemble: {
-  		files: ['<%= config.src %>/{content,data,templates,css/scss,js}/{,*/}*.{md,hbs,yml,scss,js}'],
-  		tasks: ['build']
-  	},
-  	livereload: {
-  		options: {
-  			livereload: '<%= connect.options.livereload %>'
-  		},
-  		files: [
-  			'<%= config.dist %>/{,*/}*.html',
-  			'<%= config.dist %>/{,*/}*.scss',
-				'<%= config.dist %>/{,*/}*.js',
-				'<%= config.dist %>/assets/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
-				]
-			}
-		},
+    watch: {
+      assemble: {
+      files: ['<%= config.src %>/{content,data,templates,css/scss,js}/{,*/}*.{md,hbs,yml,scss,js}'],
+      tasks: ['build']
+    },
 
-		cssmin: {
-			target: {
-				files: [{
-					expand: true,
-					cwd: '<%= config.src %>/css/build',
-					src: ['*.css', '!*.min.css'],
-					dest: '<%= config.src %>/css/build',
-					ext: '.min.css'
-				}]
-			}
-		},
-
-		autoprefixer: {
-			dev: {
-				files: {
-					'<%= config.dist %>/css/styles.css': ['<%= config.src %>/css/build/styles.css']
-				}
-			}
-		},
-
-		connect: {
-			options: {
-				port: 9001,
-				livereload: 35729,
-		    // change this to '0.0.0.0' to access the server from outside
-		    hostname: 'localhost'
-		  },
-		  livereload: {
-		  	options: {
-		  		open: true,
-		  		base: [
-		  		'<%= config.dist %>'
-		  		]
-		  	}
-		  }
-		},
-
-    copy: {
-      images: {
-        files: [
-          {
-            expand: true,
-            cwd: '<%= config.src %>/images/',
-            src: ['**/*.{png,jpg,svg}'],
-            dest:'<%= config.dist %>/mimages/'
-          }
-        ]
+    livereload: {
+      options: {
+        livereload: '<%= connect.options.livereload %>'
       },
+      files: [
+        '<%= config.dist %>/{,*/}*.html',
+        '<%= config.dist %>/{,*/}*.scss',
+        '<%= config.dist %>/{,*/}*.js',
+        '<%= config.dist %>/assets/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+      ]
+    }
+  },
 
-      scripts: {
-        files: [
-          {
-            expand: true,
-            cwd: '<%= config.src %>/js/vendor',
-            src: ['**/*.js'],
-            dest:'<%= config.dist %>/js/vendor/'
-          }
-        ]
+cssmin: {
+  target: {
+    files: [{
+      expand: true,
+      cwd: '<%= config.src %>/css/build',
+      src: ['*.css', '!*.min.css'],
+      dest: '<%= config.src %>/css/build',
+      ext: '.min.css'
+    }]
+  }
+},
+
+autoprefixer: {
+  dev: {
+    files: {
+      '<%= config.src %>/css/build/styles.css': ['<%= config.src %>/css/build/styles.css']
+    }
+  }
+},
+
+connect: {
+  options: {
+    port: 9000,
+    livereload: 35729,
+        // change this to '0.0.0.0' to access the server from outside
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: [
+          '<%= config.dist %>'
+          ]
+        }
       }
     },
-	});
 
-	grunt.registerTask('default', [
-		'build',
-		'connect:livereload',
-		'watch'
-	]);
+    assemble: {
+      pages: {
+        options: {
+          expand: true,
+          assets: '<%= config.dist %>/assets',
+          layout: 'default.hbs',
+          layoutdir: '<%= config.src %>/templates/layouts/',
+          data: '<%= config.src %>/data/*.{json,yml}',
+        partials: '<%= config.src %>/templates/partials/{,*/}*.hbs'
+      },
+      files: [{
+        expand: true,
+        cwd: '<%= config.src %>/templates/pages',
+        src: ['**/*.hbs'],
+        dest: '<%= config.dist %>'
+      }]
+    }
+  },
 
-	grunt.registerTask('build', [
-		'concat',
-		'uglify',
-		'sass',
-		'autoprefixer',
-		'cssmin',
-		'copy'
+  copy: {
+    theme: {
+      expand: true,
+      cwd: '<%= config.src %>/css/',
+      src: '**/*.{css,eot,ttf,woff,svg}',
+      dest: '<%= config.dist %>/css/'
+    },
+    images: {
+      expand: true,
+      cwd: '<%= config.src %>/templates/assets/images',
+      src: '**',
+      dest: '<%= config.dist %>/images'
+    },
+    video: {
+      expand: true,
+      flatten: true,
+      cwd: '<%= config.src %>/templates/assets/video',
+      src: '**',
+      dest: '<%= config.dist %>/video'
+    },
+    audio: {
+      expand: true,
+      flatten: true,
+      cwd: '<%= config.src %>/templates/assets/audio',
+      src: '**',
+      dest: '<%= config.dist %>/audio'
+    }
+  },
+
+    // Before generating any new files,
+    // remove any previously-created files.
+    clean: ['<%= config.dist %>/**/*.{html,xml}']
+
+  });
+
+grunt.loadNpmTasks('assemble');
+
+grunt.registerTask('default', [
+  'build',
+  'connect:livereload',
+  'watch'
   ]);
+
+grunt.registerTask('build', [
+  'clean',
+  'concat',
+  'uglify',
+  'sass',
+  'autoprefixer',
+  'cssmin',
+  'copy',
+  'assemble'
+  ]);
+
 };
