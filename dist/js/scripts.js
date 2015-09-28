67ebd17eb0783ba5cd5e04e93d3b01218ecdff2f
quattromani <max@maxquattromani.com>
2015-09-27 21:59:15 -0600

    added room to scroll-to

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -148,7 +148,7 @@ $('.fonts').each(function(){
     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
      if (target.length) {
        $('html,body').animate({
-           scrollTop: target.offset().top
+           scrollTop: target.offset().top - 20
       }, 1000);
       return false;
     }
