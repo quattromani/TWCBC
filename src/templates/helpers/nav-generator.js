a977b61c446ed9ed0c9590831c2ddf04ed319db1
jeffolsen <olsenjeffn@gmail.com>
2015-10-12 16:46:12 -0600

    add lodash and start nav-generator

--- src/templates/helpers/nav-generator.js
+++ src/templates/helpers/nav-generator.js
@@ -1,9 +1,45 @@
+var _ = require('lodash');
+
 module.exports.register = function(Handlebars, options, params) {
-  Handlebars.registerHelper('read_data', function(context, options) {
+  Handlebars.registerHelper('read_data', function(data, options) {
     console.log("Jeff's helper called");
-    // console.log(options);
-    console.log(context);
-    // console.log("the stuff", params.assemble.options);
+    // console.log(JSON.stringify(data, null, '  '));
+
+
+    function traverseTo(dataset, title) {
+      var keys = _.keys(dataset);
+
+      for (var key in keys) {
+        var list = dataset[keys[key]];
+
+        if (Array.isArray(list)) {
+          console.log("is list");
+
+          for (var i = 0; i < list.length; i++) {
+            var entry = list[i];
+            console.log(entry);
+
+            if (!!entry.title) {
+              console.log(entry.title);
+
+              if (entry.title === title) {
+                return entry;
+              }
+
+              return traverseTo(entry, title);
+            }
+
+            console.log("no item with title " + title + " found");
+          }
+        }
+      }
+    }
+
+    traverseTo(data, "Toolkit");
+    // console.log(JSON.stringify(traverseTo(data, "Toolkit"), null, '  '));
+
+
+
     // return options.fn(this);
   });
-}
+};
