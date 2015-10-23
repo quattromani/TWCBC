f16cdadfa790f65718b2cc68180eaff01961db91
quattromani <max@maxquattromani.com>
2015-10-23 12:41:12 -0600

    updated homepage content

--- src/templates/helpers/toLowerCase.js
+++ src/templates/helpers/toLowerCase.js
@@ -1,4 +1,8 @@
-Handlebars.registerHelper('toLowerCase', function(value) {
- return (value && _.isString(value)) ? value.toLowerCase() : '';
-});
+var _ = require('lodash');
+
+module.exports.register = function(Handlebars, options, params) {
+  Handlebars.registerHelper('toLowerCase', function(value) {
+    return (value && _.isString(value)) ? value.toLowerCase() : '';
+  });
+};
 // usage: {{toLowerCase someString}}
