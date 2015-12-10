3933ff5700c9978b0a2332477f1caf6caf3f6157
quattromani <max@maxquattromani.com>
2015-12-09 20:09:43 -0700

    adding some room to headerheight

--- js/scripts.js
+++ js/scripts.js
@@ -112,7 +112,7 @@ $('.fonts').each(function(){
 headerHeight = $('header[role=banner]').height();
 
 $(function() {
-  $('.twc-bc-container').css('margin-top',headerHeight);
+  $('.twc-bc-container').css('margin-top', headerHeight + 20);
   $('aside').css("cssText", "height: " + headerHeight + " !important;");
 });
 ;/* ==========================================================================
