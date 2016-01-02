c54f977bd18c73ca793334c260fdea91e2923baa
jeffolsen <olsenjeffn@gmail.com>
2016-01-02 10:34:43 -0700

    add active state to primary nav

---
+++ src/templates/helpers/on-path.js
@@ -0,0 +1,22 @@
+var _ = require('lodash');
+
+module.exports.register = function(Handlebars, options, params) {
+  Handlebars.registerHelper('on-path', function(this_page, path_name, options) {
+    var path = this_page.page.dirname.split("/");
+    var onpath = false;
+
+    if (this_page.page.basename === path_name) {
+      return true;
+    }
+
+    for (var i = 0; i < path.length; i++) {
+      // console.log("Path: ", path[i]);
+      if (path[i] === path_name) {
+        onpath = true;
+        break;
+      }
+    }
+
+    return onpath;
+  });
+};
