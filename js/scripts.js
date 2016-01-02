831d0baa81de5d3f93802bfbb2f71ab42d104ade
quattromani <max@maxquattromani.com>
2016-01-01 22:36:23 -0700

    calling it a night

--- js/scripts.js
+++ js/scripts.js
@@ -144,6 +144,15 @@ $('.accordion .expandable h6').click(function() {
 	});
 });
 ;/* ==========================================================================
+    Alerts -- Version: 1.9.0.0 - Updated: 12/31/2013
+   ========================================================================== */
+
+$('.twc-alert .twc-alert-text:first').prepend('<a class="twc-icon twc-alert-close twc-show_hide" href="" style=""></a>');
+$('.twc-show_hide').click(function(e) {
+  $(this).parent().slideToggle();
+  e.preventDefault();
+});
+;/* ==========================================================================
     Button Checkbox -- Version: 1.9.0.1 - Updated: 2/4/2014
    ========================================================================== */
 
