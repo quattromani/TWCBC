5e1f208f483179945a217091a3454f43998e9589
jeffolsen <olsenjeffn@gmail.com>
2016-01-16 19:21:59 -0700

    add scroll highlighter

--- js/scripts.js
+++ js/scripts.js
@@ -295,6 +295,40 @@ $('#marquee-left').click(function() {
     }
   }
 });
+
+var jump_anchors = $(".jumpTo-anchor");
+
+$(window).scroll(function() {
+  var offset = $(window).scrollTop();
+  var current_anchor;
+
+  for (var i = 0; i < jump_anchors.length; i++) {
+    var this_anchor = $(jump_anchors).eq(i);
+
+    if (this_anchor.offset().top - offset > 450) {
+      break;
+    }
+
+    current_anchor = this_anchor;
+  }
+
+  var nav_target = $('a[href*="#' + current_anchor.attr('id') + '"]');
+
+  if (nav_target.parent().parent().parent().hasClass('js-expandable')) {
+    nav_target = nav_target.parent().parent().parent();
+  } else {
+    nav_target = nav_target.parent();
+  }
+
+  if (!nav_target.hasClass('js-active')) {
+    console.log("changing because " + current_anchor.attr('id'));
+
+    $('aside .accordion .js-expandable ul').slideUp();
+    $('aside .accordion li').removeClass("js-active js-open");
+    nav_target.find('ul').slideDown();
+    nav_target.addClass("js-active");
+  }
+});
 ;/* ==========================================================================
     Tabs to Accordion -- Version: 1.9.0.2 - Updated: 1/7/2013
    ========================================================================== */
